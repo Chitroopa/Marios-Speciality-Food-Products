9ae04c83b762ceeaf1403b6efc5974aac280460c
Chitroopa Manikkavasagam <chitroopa72193@gmail.com>
2017-06-30 10:38:42 -0700

    Add bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,4 @@
 Rails.application.routes.draw do
-  get 'reviews/index'
-
-  get 'reviews/edit'
-
-  get 'reviews/show'
-
-  get 'reviews/new'
-
-  get 'products/index'
-
-  get 'products/edit'
-
-  get 'products/show'
-
-  get 'products/new'
-
   root :to => 'products#index'
   resources :products do
     resources :reviews
