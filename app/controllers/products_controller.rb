217a2152aeb629a97fa0d1d4b9b4e8f2a2f27ed6
Chitroopa Manikkavasagam <chitroopa72193@gmail.com>
2017-06-30 11:59:19 -0700

    Add new product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,8 +7,27 @@ class ProductsController < ApplicationController
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 
   def new
+    @product = Product.new
   end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "New product added successfully!"
+      redirect_to  product_path(@product)
+    else
+      render :new
+    end
+  end
+
+private
+
+  def product_params
+    params.require(:product).permit(:name,:cost,:country)
+  end
+
 end
